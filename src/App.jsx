433dbbbf46a256c8af81859ea5d265f7a3c182aa
Johnrome Pangon <gomegxxiv@gmail.com>
2024-06-05 18:27:50 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import GroceryComponent from "./components/GroceryComponent";
+
+function App() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-purple-500 via-indigo-500 to-pink-500">
+      <GroceryComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GroceryComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const GroceryComponent = () => {
+  return (
+    <div>
+      <h1>Grocery Buddy</h1>
+      <form>
+        <input type="text" placeholder="add item..." />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default GroceryComponent;
